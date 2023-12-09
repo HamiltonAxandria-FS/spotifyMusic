8ab21bf6e99ce6f580b89daaeb36d784976b2b9a
Axandria Hamilton <amhamilton1@student.fullsail.edu>
2023-12-09 13:44:18 -0500

    fixed token

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
-const routes = require("./routes/routes");
+const authRouter = require("./routes/routes");
 require("dotenv").config();
 
-const app = express();
 const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
 
-
+//app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,8 +21,9 @@ db.once("open", () => {
   console.log("Database Connection Established");
 });
 
-app.use("/auth", routes);
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,14 +2,18 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const { checkToken, search } = require("../controllers/route_controller");
-
-
 require("dotenv").config();
 
 const clientSecret = process.env.clientSecret
 const clientID = process.env.clientID
-const redirectUri = process.env.redirectUri
+const REDIRECT_URI = process.env.redirectUri
 const tokenUrl = "https://accounts.spotify.com/api/token";
+
+
+
+
+console.log('from routes')
+
 
 function generateRandomString(length) {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -20,23 +24,24 @@ function generateRandomString(length) {
     return result;
   }
 
+
+  // login first 
 router.get("/login", (req, res) => {
-    const state = generateRandomString(16);
-    const scope = 'user-read-private user-read-email';
+    console.log('attempting login');
     const clientID = process.env.clientID
 
   const queryParams = new URLSearchParams({
     response_type: "code",
     client_id: clientID,
-    scope: scope,
-    redirect_uri: redirectUri,
-    state: state
   });
 
-  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}&redirect_uri=${REDIRECT_URI}`);
 });
 
 
+
+// post login come here (2nd)
 router.get("/callback", async (req, res) => {
   const code = req.query.code;
 
@@ -45,25 +50,36 @@ router.get("/callback", async (req, res) => {
   }
 
   try {
-    const response = await axios.post(tokenUrl, {
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: redirectUri,
-      client_id: clientID,
-      client_secret: clientSecret,
+    const response = await axios({
+      method: 'POST',
+      url: 'https://accounts.spotify.com/api/token',
+      params: {
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: REDIRECT_URI,
+      },
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${clientID}:${clientSecret}`
+        ).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
     });
-
-    res.redirect(redirectUri);
+    console.log(response.data);
   } catch (error) {
     console.error("Error exchanging code for access token:", error);
     res.status(500).send("Internal Server Error");
   }
 });
 
+
+// checks that token ttl is still valid 
 router.get('/status', (req, res) => {
     checkToken(req, res);
 })
 
+
+// extra steps
 router.get('/search', (req, res) => {
     search(req, res);
 })
